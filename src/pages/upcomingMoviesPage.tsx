0678ccb319f29340d3c410e8897b16355d632a23
Vincent Byrne <vincentpbyrne@hotmail.com>
2025-08-25 18:31:58 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -1,32 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from '../components/templateMovieListPage';
 import { BaseMovieProps } from "../types/interfaces";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import { DiscoverMovies } from "../types/interfaces";
+import { useQuery } from "react-query";
+import Spinner from "../components/spinner";
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites';
 
 const UpcomingMoviesPage: React.FC = () => {
-  const [movies, setMovies] = useState<BaseMovieProps[]>([]);
-  const favourites = movies.filter(m => m.favourite)
-  localStorage.setItem('favourites', JSON.stringify(favourites))
+  const { data, error, isLoading, isError } = useQuery<DiscoverMovies, Error>(
+    "upcoming", 
+    getUpcomingMovies
+  );
 
-  const addToFavourites = (movieId: number) => {
-    const updatedMovies = movies.map((m: BaseMovieProps) =>
-      m.id === movieId ? { ...m, favourite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+    if (isLoading) {
+    return <Spinner />;
+  }
 
-  useEffect(() => {
-    getUpcomingMovies().then((movies) => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
 
-  return (
+   return (
     <PageTemplate
       title="Upcoming Movies"
-      movies={movies}
-      selectFavourite={addToFavourites}
+      movies={data?.results || []}
+      action={(movie: BaseMovieProps) => {
+        return <AddToFavouritesIcon {...movie} />
+      }}
     />
   );
 };
